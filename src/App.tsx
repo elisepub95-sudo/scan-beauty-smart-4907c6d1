b25c448b04e52af27bd328653addce7054e62f9b
elisepub95-sudo <elise.pub95@gmail.com>
2025-11-24 22:36:14 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
+
 import Auth from "./pages/Auth";
 import Home from "./pages/Home";
 import Scanner from "./pages/Scanner";
@@ -34,7 +35,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
@@ -58,10 +60,11 @@ const App = () => (
           <Route path="/admin/routines" element={<AdminRoutines />} />
           <Route path="/admin/products" element={<AdminProducts />} />
           <Route path="/admin/diagnostics" element={<AdminDiagnostics />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+
           <Route path="*" element={<NotFound />} />
         </Routes>
-      <HashRouter>
+      </HashRouter>
+
     </TooltipProvider>
   </QueryClientProvider>
 );
